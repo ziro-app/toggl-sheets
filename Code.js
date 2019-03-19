431921c8af6e902d83308fef40ac4b7fdffdcc36
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-03-18 17:09:26 -0700

    add since param to api request

--- Code.js
+++ Code.js
@@ -1,10 +1,12 @@
 var token = PropertiesService.getScriptProperties().getProperty('TOGGL_TOKEN')
-var url = PropertiesService.getScriptProperties().getProperty('TOGGL_URL')
+var root_url = PropertiesService.getScriptProperties().getProperty('TOGGL_URL')
 var options = {'headers': {'Authorization': 'Basic ' + Utilities.base64Encode(token + ':api_token')}}
-
+var date = new Date()
+var since = new Date(date.setDate(date.getDate() - date.getDay())).toISOString()
 
 function TOGGLREPORT(workspace) {
- 	var projects = JSON.parse(UrlFetchApp.fetch(url + workspace, options).getContentText()).data
+	var url = root_url + workspace + "&since=" + since
+ 	var projects = JSON.parse(UrlFetchApp.fetch(url, options).getContentText()).data
  	var ordersTitlesAndTimes = projects.map(function(project) {
  		var order = project.title.project.split(" | ")[0]
  		var title = project.title.project.split(" | ")[1]
